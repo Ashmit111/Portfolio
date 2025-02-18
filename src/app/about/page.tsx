d0d0b193e9d8079ccddf7e52c945f01b6d02687b
Ashmit111 <ashmitsingh862@gmail.com>
2025-02-18 23:34:58 +0530

    update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -22,7 +22,7 @@ const Page: React.FC = () => {
             Hey I&apos;m <b className="text-cyan-300">Ashmit</b>
           </p>
           <TypewriterEffect
-            text="A 3rd-year engineering student from Mumbai and a self-taught Full-Stack Developer who loves bringing websites to life. I enjoy building complex backend systems while also crafting engaging and visually appealing frontends. Beyond coding, I love reading books and travelling. Currently, I’m exploring new technologies in Web3 and sharpening my problem-solving skills through competitive programming. You can find my tech stack below! 🚀"
+            text="A 3rd-year engineering student from Mumbai and a self-taught Full-Stack Developer who loves bringing websites to life. I enjoy building complex backend systems while also crafting engaging and visually appealing frontends. Beyond coding, I love reading books and travelling. Currently, I’m exploring new technologies in Web3 and sharpening my problem-solving skills through competitive programming 🚀"
             delay={300}
             className="font-mono text-gray-400 pt-4"
           />
